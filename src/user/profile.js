764e67655105312026c183ec3c57fa540e1b9d85
Matthew Cadena <mcadena@andrew.cmu.edu>
2026-02-04 03:35:14 +0000

    Add helper function to reduce changePassword function's complexity

--- src/user/profile.js
+++ src/user/profile.js
@@ -338,10 +338,7 @@ module.exports = function (User) {
 		}
 	}
 
-	User.changePassword = async function (uid, data) {
-		if (uid <= 0 || !data || !data.uid) {
-			throw new Error('[[error:invalid-uid]]');
-		}
+	async function validateNewPassword(uid, data) {
 		User.isPasswordValid(data.newPassword);
 		const [isAdmin, hasPassword] = await Promise.all([
 			User.isAdministrator(uid),
@@ -369,6 +366,14 @@ module.exports = function (User) {
 				throw new Error('[[user:change-password-error-same-password]]');
 			}
 		}
+	}
+
+	User.changePassword = async function (uid, data) {
+		if (uid <= 0 || !data || !data.uid) {
+			throw new Error('[[error:invalid-uid]]');
+		}
+
+		await validateNewPassword(uid, data);
 
 		const hashedPassword = await User.hashPassword(data.newPassword);
 		await Promise.all([
