829d547ccc5d5708d6b8ff8c76efd0b28dd00b2b
Aaron Lockhart <aaronlockhartdev@gmail.com>
2026-02-25 18:46:06 -0500

    Added message if all posts in a topic are private

--- src/topics/posts.js
+++ src/topics/posts.js
@@ -161,7 +161,14 @@ module.exports = function (Topics) {
 
 	Topics.modifyPostsByPrivilege = function (topicData, topicPrivileges) {
 		const loggedIn = parseInt(topicPrivileges.uid, 10) > 0;
+		const allPostsPrivate = topicData.posts.length > 0 && topicData.posts.every(post => post.modOnly);
+		const allVisiblePostsPrivate = topicData.posts.filter(post => post && (!post.modOnly || topicPrivileges.isAdminOrMod)).length === 0 && allPostsPrivate;
 		topicData.posts = topicData.posts.filter(post => post && (!post.modOnly || topicPrivileges.isAdminOrMod));
+		
+		if (allVisiblePostsPrivate && !topicPrivileges.isAdminOrMod) {
+			topicData.allPostsPrivate = true;
+		}
+		
 		topicData.posts.forEach((post) => {
 			if (post) {
 				post.topicOwnerPost = parseInt(topicData.uid, 10) === parseInt(post.uid, 10);
