47b7ab58519feae6cfd5fa9f54515bcedf8b0350
Matthew Cadena <mcadena@andrew.cmu.edu>
2026-03-26 02:13:39 +0000

    feat(ui): add translation button starter code for P4

--- src/posts/data.js
+++ src/posts/data.js
@@ -80,6 +80,13 @@ function modifyPost(post, fields) {
 			}
 		}
 
+		// Mark post as "English" if decided by translator service or if it has no info
+		post.isEnglish = post.isEnglish == 'true' || post.isEnglish === undefined;
+		// If translatedContent is undefined, default to empty string (no translation needed for English posts)
+		if (post.translatedContent === undefined) {
+			post.translatedContent = '';
+		}
+
 		if (typeof post.postType === 'undefined' || post.postType === null) {
 			post.postType = Posts.DEFAULT_POST_TYPE;
 		}
