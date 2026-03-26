c2330f5466a910a6a0330ecbcfb0d1fbc9e00288
Matthew Cadena <mcadena@andrew.cmu.edu>
2026-03-26 18:47:11 +0000

    feat(translation): switch from dummy call to microservice call

--- src/translate/index.js
+++ src/translate/index.js
@@ -4,14 +4,17 @@
 
 const translatorApi = module.exports;
 
-translatorApi.translate = function (postData) {
-	return ['is_english',postData];
-};
-
-// translatorApi.translate = async function (postData) {
-//  Edit the translator URL below
-//  const TRANSLATOR_API = "TODO"
-//  const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
-//  const data = await response.json();
-//  return ['is_english','translated_content'];
+// translatorApi.translate = function (postData) {
+// return ['is_english', postData.content];
 // };
+
+translatorApi.translate = async function (postData) {
+	const TRANSLATOR_API = 'http://localhost:5000';
+	try {
+		const response = await fetch(TRANSLATOR_API + '/?content=' + encodeURIComponent(postData.content));
+		const data = await response.json();
+		return [data.is_english, data.translated_content];
+	} catch (e) {
+		return [true, postData.content];
+	}
+};
